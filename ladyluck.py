3337ff1d9141cab4e05652c2b023c9ab70b196d5
TrshPnda <73556871+TrshPnda@users.noreply.github.com>
2021-01-05 13:29:43 -0500

    Update ladyluck.py

--- ladyluck.py
+++ ladyluck.py
@@ -6,25 +6,46 @@ import requests
 import random
 from lxml import html # Look into using re module instead
 
-def login(otp):
+
+def optgen(size=6, chars=string.digits): ##random otp generator
+  return ''.join(random.choice(chars) for x in range(size))
+
+def login():
+  attempts = 0
+  counter = 0
+  while counter == 0:
+    otp = optgen()
+    s = requests.Session()
 '''
 Login Process goes here. 
 Thinking of a way to handle different types of logins - 302s, 403s etc
 200 and 403 status would be the easiest to implement. 
 302 type flows would require a little more thought. Requires conditionals.
 def a different function for webapps with crsf tokens? hm.
-'''
-  if:
-    print(result3.cookies['']) #name of cookie variable
-  else:
-    pass
+'''    
+    if "" in result.text:
+      print(result.cookies['']) #Session Cookie Name
+      counter += 1
+      return attempts
+      return counter
+    else:
+      pass
+      attempts +=1
 
-def optgen(size=6, chars=string.digits):
-  return ''.join(random.choice(chars) for x in range(size))
-  
-def main:
-  while counter == 0 
-  otp = otpgen()
-  login(otp)
+##Attempting to multi thread###
+
+threads = []
+
+if __name__ == '__main__':
+  for i in range (1, 20) :
+    worker = login(i) 
+    worker.setDaemon(True)
+    worker.start()
+    threads.append(worker)
+  if counter == 1:
+    for item in threads:
+      item.join()
+  print ("This took %d attempts"%attempts)
+
 ## Something like that ##
 
